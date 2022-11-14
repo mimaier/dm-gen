b6a23b706d2db531dc9ed5dad1cefd6695fdd412
Fabiankuch <fabiankuchling@gmail.com>
2022-11-14 15:11:25 +0100

    login verification

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -23,6 +23,18 @@ export class SignUpComponent implements OnInit {
     window.location.reload();
   }
   loginUser(email:string, password:string){
+    let signInResponse : String;
+    const signInSuccessLabel = document.getElementById('sign-in-success') as HTMLInputElement;
+    console.log("in loginUser");
+    if(email.includes("@") == false){
+      console.log("inlcudes NO @");
+      signInSuccessLabel.style.visibility = "visible";
+      signInSuccessLabel.style.color = "red";
+      signInSuccessLabel.innerHTML = "This is not a valid email address";
+      return("This is not a valid email address");   
+    }
+    console.log("inlcudes @");
+
     this.user.loginUser(email, password).subscribe(data=>{
       this.data = Object.values (data);
       console.log(this.data)
@@ -32,33 +44,61 @@ export class SignUpComponent implements OnInit {
       localStorage.setItem('token', this.data[3]);
       localStorage.setItem('usergenerations', this.data[4]);
       localStorage.setItem('userfreegenerations', this.data[5]);
-      const accountButton = document.getElementById('accountbtn') as HTMLInputElement;
-      const logoutButton = document.getElementById('logoutbtn') as HTMLInputElement;
-      const registerButton = document.getElementById('registerbutton') as HTMLInputElement;
+      localStorage.setItem('userloginable', this.data[6]);
 
-      console.log(accountButton);
-      accountButton.style.display = "block";
-      logoutButton.style.display = "block";
-      registerButton.style.display = "none";
+      if(localStorage.getItem('userloginable') == "1"){
+        const accountButton = document.getElementById('accountbtn') as HTMLInputElement;
+        const logoutButton = document.getElementById('logoutbtn') as HTMLInputElement;
+        const registerButton = document.getElementById('registerbutton') as HTMLInputElement;
+  
+        console.log(accountButton);
+        accountButton.style.display = "block";
+        logoutButton.style.display = "block";
+        registerButton.style.display = "none";
 
-      const signInSuccessLabel = document.getElementById('sign-in-success') as HTMLInputElement;
-      signInSuccessLabel.style.visibility = "visible";
-      this.navigateToPage('/');
-    })
+        this.navigateToPage('/');
+      }else{
+        signInSuccessLabel.style.visibility = "visible";
+        signInSuccessLabel.style.color = "#ff5266";
+        signInSuccessLabel.innerHTML = "Check your email to activate your account";
+      }
+
+    },
+    err => {
+        signInSuccessLabel.style.visibility = "visible";
+        signInSuccessLabel.style.color = "#ff5266";
+        signInSuccessLabel.innerHTML = "Email or Password wrong";
+    });
+
+    return("Login was successful");
+
   }
 
   registerUser(username:string, password:string, email:string){
+    const signUpSuccessLabel = document.getElementById('sign-up-success') as HTMLInputElement;
+    console.log("in loginUser");
+    if(email.includes("@") == false){
+      console.log("inlcudes NO @");
+      signUpSuccessLabel.style.visibility = "visible";
+      signUpSuccessLabel.style.color = "#ff5266";
+      signUpSuccessLabel.innerHTML = "This is not a valid email address";
+      return("This is not a valid email address");   
+    }
 
     this.user.registerUser(username, password, email).subscribe(data=>{
       this.data = Object.values (data);
       console.log(this.data)
       const signUpSuccessLabel = document.getElementById('sign-up-success') as HTMLInputElement;
       console.log(signUpSuccessLabel);
+      signUpSuccessLabel.style.color = "#47fc77";
+      signUpSuccessLabel.innerHTML = "Successfully registered! <br>Please check your email account";
       signUpSuccessLabel.style.visibility = "visible";
 
      // this.loginUser(email, password);
 
     })
+    return("Register was successful");
+
   }
 
 }
